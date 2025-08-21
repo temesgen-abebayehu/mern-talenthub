5c03dbb3677500261f1a1018e773d8561333bd39
temesgen-abebayehu <temesgensoftware21@gmail.com>
2025-08-22 02:27:41 +0300

    frontend  setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF',
+        secondary: '#10B981'
+      }
+    },
+  },
+  plugins: [],
+}
